e8cf15f82e940fc21e9141df26e3d97c2b0e8748
niceoasi <nicebuvr@naver.com>
2021-10-02 12:12:33 +0900

    Initialize Project

---
+++ DaengnyangDiary/DaengnyangDiary/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  DaengnyangDiary
+//
+//  Created by Ethan on 2021/10/02.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ DaengnyangDiary/DaengnyangDiary/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  DaengnyangDiary
+//
+//  Created by Ethan on 2021/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
